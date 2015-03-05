09ac3906bfe085e2f5d47e05351cfa881374260a
Josh <joshpachan@gmail.com>
2015-03-04 21:38:18 -0500

    Write interface to tts

--- castle/speech/tts.py
+++ castle/speech/tts.py
@@ -14,8 +14,8 @@ I said, "How was your day?".
 
 imported:
 >>>import tts
->>>speaker = tts.Speaker()
->>>speaker.say("How was your day?")
+>>>Morgan_Freeman = tts.Speaker()
+>>>Morgan_Freeman.say("How was your day?")
 I said, "How was your day?"
 >>>
 """
@@ -24,9 +24,10 @@ I said, "How was your day?"
 import subprocess # to open ffmpeg from bin
 
 # Use some TTS service before I roll my own
-import requests # to make internet call
+import requests # to make API call
 
 import queue # to hold texts to say
+
 # Process
 # import numpy # to do necessary numerical computations
 
@@ -37,8 +38,7 @@ class Speaker(object):
 
 	def __init__(self):
 		max_sents = 12
-		self.q = queue.Queue(max_sents) # Holds the text queue
-		pass
+		self.say_q = queue.Queue(max_sents) # Holds the text queue
 
 	def get_queue(self):
 		"""
@@ -54,14 +54,38 @@ class Speaker(object):
 		Puts it through a pre-processor and (temporarily) hits the TTS API with it.
 		"""
 		uri = "http://tts-api.com/tts.mp3"
-		query = "q=hello+world"
 
-		raise NotImplementedError
+		resp = requests.get(uri, text)
+		print(resp.url)
 		return self
 
 	def _say(self, audio_data):
-		# Spawn subprocess of fmmpeg
 		# Pop speech from Q, blocks
+
+		# Spawn subprocess of fmmpeg
+		p = subprocess.Popen('./bin/ffplay', '-i', stdin=subprocess.PIPE)
+
+		p.communicate(audio_data)
 		# Pipes text to ffmpeg
 		# When ffmpeg finishes, unblock Q
 		return self
+
+
+def test_say(speaker):
+	speaker.say("Hello, my name is Baymax.")
+	speaker.say("I activated because you are in pain.")
+	speaker.say("I will scan you now.")
+	speaker.say("Are you satisfied with your care?")
+
+def main():
+	baymax = Speaker()
+
+	baymax.activate()
+	# test_say(baymax)
+
+	baymax.poll()
+	baymax.deactivate()
+
+
+if __name__ == "__main__":
+	main()
